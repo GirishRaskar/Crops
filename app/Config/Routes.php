f846b1e33d4af2d26237c6ef3c00e3a7715de8ce
unknown <raskargirish23@gmail.com>
2025-07-02 18:44:54 +0530

    updated data for pdf release of 30 JUNE

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'CropController::index');
+
+$routes->get('/api', 'CropController::test_upag_api');
--- app/Controllers/CropController.php
+++ app/Controllers/CropController.php
@@ -9,41 +9,41 @@ class CropController extends BaseController
 
     public function __construct()
     {
-        $this->lastUpdated = "27 June 2025"; 
-        $this->date_of_Data = "20 June 2025";
+        $this->lastUpdated = "2 July 2025"; 
+        $this->date_of_Data = "27 June 2025";
 
        $rawCrops = [
             "Pulses" => [
-                ["name" => "Arhar",        "area" => 2.48, "last_year" => 2.61, "normal" => 44.71],
-                ["name" => "Urdbean",      "area" => 1.39, "last_year" => 0.62, "normal" => 32.64],
-                ["name" => "Moongbean",    "area" => 4.43, "last_year" => 2.67, "normal" => 35.69],
-                ["name" => "Kulthi",       "area" => 0.08, "last_year" => 0.07, "normal" => 1.72],
-                ["name" => "Mothbean",     "area" => 0.11, "last_year" => 0.00, "normal" => 9.70],
-                ["name" => "Other pulses", "area" => 0.94, "last_year" => 0.67, "normal" => 5.15]
+                ["name" => "Arhar",        "area" => 8.35, "last_year" => 8.67, "normal" => 44.71],
+                ["name" => "Urdbean",      "area" => 2.35, "last_year" => 1.42, "normal" => 32.64],
+                ["name" => "Moongbean",    "area" => 8.58, "last_year" => 4.30, "normal" => 35.69],
+                ["name" => "Kulthi",       "area" => 0.09, "last_year" => 0.07, "normal" => 1.72],
+                ["name" => "Mothbean",     "area" => 0.56, "last_year" => 0.01, "normal" => 9.70],
+                ["name" => "Other pulses", "area" => 1.17, "last_year" => 0.89, "normal" => 5.15]
             ],
 
             "Cereals" => [
-                ["name" => "Rice",          "area" => 13.22, "last_year" => 8.37, "normal" => 403.09],
-                ["name" => "Jowar",         "area" => 1.51,  "last_year" => 0.90, "normal" => 15.07],
-                ["name" => "Bajra",         "area" => 3.70,  "last_year" => 2.71, "normal" => 70.69],
-                ["name" => "Ragi",          "area" => 0.03,  "last_year" => 0.32, "normal" => 11.52],
-                ["name" => "Small millets", "area" => 0.47,  "last_year" => 0.55, "normal" => 4.48],
-                ["name" => "Maize",         "area" => 12.32, "last_year" => 10.31, "normal" => 78.95]
+                ["name" => "Rice",          "area" => 35.02, "last_year" => 23.78, "normal" => 403.09],
+                ["name" => "Jowar",         "area" => 2.70,  "last_year" => 1.53, "normal" => 15.07],
+                ["name" => "Bajra",         "area" => 14.76,  "last_year" => 10.40, "normal" => 70.69],
+                ["name" => "Ragi",          "area" => 0.08,  "last_year" => 0.91, "normal" => 11.52],
+                ["name" => "Small millets", "area" => 0.53,  "last_year" => 0.82, "normal" => 4.48],
+                ["name" => "Maize",         "area" => 23.69, "last_year" => 21.35, "normal" => 78.95]
             ],
 
             "Oilseeds" => [
-                ["name" => "Groundnut",      "area" => 1.78, "last_year" => 1.91, "normal" => 45.10],
-                ["name" => "Soybean",        "area" => 3.07, "last_year" => 3.12, "normal" => 127.19],
-                ["name" => "Sunflower",      "area" => 0.27, "last_year" => 0.26, "normal" => 1.29],
-                ["name" => "Sesamum",        "area" => 0.20, "last_year" => 0.14, "normal" => 10.32],
-                ["name" => "Castor",         "area" => 0.01, "last_year" => 0.02, "normal" => 9.65],
-                ["name" => "Other Oilseeds", "area" => 0.03, "last_year" => 0.04, "normal" => 0.00]
+                ["name" => "Groundnut",      "area" => 15.79, "last_year" => 8.14, "normal" => 45.10],
+                ["name" => "Soybean",        "area" => 32.04, "last_year" => 31.78, "normal" => 127.19],
+                ["name" => "Sunflower",      "area" => 0.39, "last_year" => 0.37, "normal" => 1.29],
+                ["name" => "Sesamum",        "area" => 0.69, "last_year" => 0.43, "normal" => 10.32],
+                ["name" => "Castor",         "area" => 0.04, "last_year" => 0.06, "normal" => 9.65],
+                ["name" => "Other Oilseeds", "area" => 0.04, "last_year" => 0.04, "normal" => 0.00]
             ],
 
             "Commercial" => [
-                ["name" => "Sugarcane",     "area" => 55.07, "last_year" => 54.88, "normal" => 52.51],
-                ["name" => "Cotton",        "area" => 31.25, "last_year" => 29.12, "normal" => 129.50],
-                ["name" => "Jute & Mesta",  "area" => 5.46,  "last_year" => 5.62,  "normal" => 6.59]
+                ["name" => "Sugarcane",     "area" => 55.16, "last_year" => 54.88, "normal" => 52.51],
+                ["name" => "Cotton",        "area" => 54.66, "last_year" => 59.97, "normal" => 129.50],
+                ["name" => "Jute & Mesta",  "area" => 5.47,  "last_year" => 5.62,  "normal" => 6.59]
             ]
         ];
 
